4aa57fd42fcef2b287b20caec551fde9e8a30326
“ashguerra” <ashguerraschool@gmail.com>
2025-02-08 18:28:23 -0500

    Added page files, header, and worked on inventory

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
+import Header from "../../components/Header";
 
 function Dashboard() { 
   return (
     <div>
+      <Header/>
       <h1>Welcome to the Dashboard</h1>
     </div>
   );
---
+++ src/pages/Reservations/Reservations.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const Reservations = () => {
+  return (
+    <div>
+      <Header/>
+      Reservations
+    </div>
+  )
+}
+
+export default Reservations
