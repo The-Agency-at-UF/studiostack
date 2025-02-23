8ee41ba75293c558f7a990f7d80d7f97c40cf741
mvcloud <valleemaeson@ufl.edu>
2025-02-23 13:13:10 -0500

    starting calendar

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { getDocs, collection, doc } from 'firebase/firestore';
+import { db } from '../../firebase/firebaseConfig';
 
-const Calendar = () => {
+const localizer = momentLocalizer(moment);
+
+const CalendarPage = () => {
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    const fetchReservations = async () => {
+      const reservationsRef = collection(db, 'reservations');
+      const querySnapshot = await getDocs(reservationsRef);
+
+      const allReservationsList = querySnapshot.docs.map(doc => ({
+        title: doc.data().name,
+        start: doc.data().startDate.toDate(),
+        end: doc.data().endDate.toDate(), 
+      }));
+
+      setReservations(allReservationsList);
+    }
+
+    fetchReservations();
+  }
+  , []);
+
   return (
-    <div>Calendar</div>
-  )
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={reservations}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: '75vh', backgroundColor: 'white'}}
+        popup
+      />
+    </div>
+  );
 }
 
-export default Calendar
+export default CalendarPage;
